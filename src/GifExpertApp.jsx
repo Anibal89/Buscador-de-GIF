fe6f8cb214432c12ed0dce636726679f3b945bf8
Luis Zapeta <luis.zapeta@plasticosuni.com>
2023-03-01 10:58:19 -0600

    Update app Gif

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid } from "./components/";
+
 
 export const GifExpertApp = () => {
 
--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -2,18 +2,16 @@ import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
-
-  const {images, isLoading} = useFetchGifs(category);
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
+      {isLoading && (<h2>...cargando</h2>)}
+
       <div className="card-grid">
         {images.map((image) => (
-          <GifItem 
-          key={image.id}
-          {...image}
-          />
+          <GifItem key={image.id} {...image} />
         ))}
       </div>
     </>
