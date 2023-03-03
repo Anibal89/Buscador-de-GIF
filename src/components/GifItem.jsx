edd49c9142f8d22f95cb14ca6930926c0761eb9a
Luis Zapeta uni <luis.zapeta@plasticosuni.com>
2023-03-03 12:33:04 -0600

    Donwload gif

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,10 +1,15 @@
+import {saveAs} from "file-saver"
 export const GifItem = ({title, url, id}) => {
-     
+    
+  const handleDowload = ()=>{
+    saveAs(url,title);
+   }
     return (
     <>
       <div className="card">
         <img src={url} alt={title} />
         <p>{title}</p>
+        <button onClick={handleDowload} type="button">Descargar</button>
       </div>
     </>
   );
--- src/helpers/getGifts.js
+++ src/helpers/getGifts.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) =>{
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=9GRccfEmLFEwNZWoLECO90Se0UvtnHyf&q=${category}&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=9GRccfEmLFEwNZWoLECO90Se0UvtnHyf&q=${category}&limit=20`
     const resp = await fetch(url);
     const {data} = await resp.json()
 
